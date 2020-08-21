21dde148fb9e6888eaf998475dbb35b405e7a4a2
Owethu99Twala <owethutwala99@gmail.com>
2020-08-21 13:55:10 +0200

    Big fives

---
+++ index.js
@@ -0,0 +1,128 @@
+
+// sound works
+
+function playl(){
+//     let lion = new Audio('http://www.acc.umu.se/~leng/elsalejon.wav');
+// lion.play();
+}
+
+
+
+function playb(){
+//     let buff = new Audio('http://soundfxcenter.com/animals/farm-animals/8d82b5_Buffalo_Moo_Sound_Effect.mp3');
+// buff.play();
+
+
+
+}
+
+
+function playlep(){
+//     let leo = new Audio('http://www.animal-sounds.org/jungle/Leopard%20growl%20animals089.wav');
+// leo.play();
+ 
+}
+
+function playe(){
+
+// let ele = new Audio('http://www.elemotion.org/wp-content/uploads/2011/09/roar.mp3');
+// ele.play();
+
+}
+
+
+function playr(){
+//     let rhino = new Audio('http://www.zar.co.za/sounds/rhino.mp3');
+// rhino.play();
+
+}
+
+// hover image effect
+
+function newr(){
+    document.getElementById("ro").src="ry.jpg"
+
+
+    let rhino = new Audio('http://www.zar.co.za/sounds/rhino.mp3');
+    rhino.play();
+    
+}
+
+function oldr(){
+    document.getElementById("ro").src="rino.jpg"
+}
+
+
+
+
+
+
+
+
+
+function newlep(){
+    document.getElementById("lepo").src="lepy.jpg"
+
+    let leo = new Audio('http://www.animal-sounds.org/jungle/Leopard%20growl%20animals089.wav');
+    leo.play();
+     
+}
+
+
+function oldlep(){
+    document.getElementById("lepo").src="leopard.jpg"
+}
+
+
+
+
+
+
+
+function newl(){
+    document.getElementById("lo").src="ly.jpg"
+    let lion = new Audio('http://www.acc.umu.se/~leng/elsalejon.wav');
+    lion.play();
+}
+
+
+function oldl(){
+    document.getElementById("lo").src="lio.jpg"
+}
+
+
+
+
+
+
+function newb(){
+    document.getElementById("bo").src="by.jpg"
+
+    let buff = new Audio('http://soundfxcenter.com/animals/farm-animals/8d82b5_Buffalo_Moo_Sound_Effect.mp3');
+    buff.play();
+    
+}
+
+
+
+function oldb(){
+    document.getElementById("bo").src="buffulo.jpg"
+}
+
+
+
+
+
+
+
+function newe(){
+    document.getElementById("eo").src="eley.jpg"
+    let ele = new Audio('http://www.elemotion.org/wp-content/uploads/2011/09/roar.mp3');
+ele.play();
+
+}
+
+
+function olde(){
+    document.getElementById("eo").src="ele.jpg"
+}
